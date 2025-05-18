91a4c6771342389abd4022951c830146dc634f29
JnrPtt <113989334+JnrPtt@users.noreply.github.com.>
2025-05-18 16:34:48 +0200

    Definición del objeto Entry y lógica de CryptoUtils.

--- src/main/java/org/example/CryptoUtils.java
+++ src/main/java/org/example/CryptoUtils.java
@@ -1,4 +1,51 @@
 package org.example;
 
+import javax.crypto.*;
+import javax.crypto.spec.*;
+import java.security.*;
+import java.security.spec.KeySpec;
+import java.util.Base64;
+
 public class CryptoUtils {
+    public static SecretKey generateKeyFromPassword(String password, String saltBase64) throws Exception {
+        byte[] salt = Base64.getDecoder().decode(saltBase64);
+
+        SecretKeyFactory factory = SecretKeyFactory.getInstance("PBKDF2WithHmacSHA256");
+        KeySpec spec = new PBEKeySpec(password.toCharArray(), salt, 65536, 256);
+        SecretKey tmp = factory.generateSecret(spec);
+
+        return new SecretKeySpec(tmp.getEncoded(), "AES");
+    }
+
+    public static String generateSalt(){
+        byte[] salt = new byte[16];
+        new SecureRandom().nextBytes(salt);
+        return Base64.getEncoder().encodeToString(salt);
+    }
+
+    public static String generateIv(){
+        byte[] iv = new byte[16];
+        new SecureRandom().nextBytes(iv);
+        return Base64.getEncoder().encodeToString(iv);
+    }
+
+    public static String encrypt(String plainText, SecretKey Key, String ivBase64) throws Exception {
+        Cipher cipher = Cipher.getInstance("AES/CBC/PKCS5Padding");
+        IvParameterSpec iv = new IvParameterSpec(Base64.getDecoder().decode(ivBase64));
+
+        cipher.init(Cipher.ENCRYPT_MODE, Key, iv);
+        byte[] cipherText = cipher.doFinal(plainText.getBytes());
+
+        return Base64.getEncoder().encodeToString(cipherText);
+    }
+
+    public static String decrypt(String cipherTextBase64, SecretKey Key, String ivBase64) throws Exception {
+        Cipher cipher = Cipher.getInstance("AES/CBC/PKCS5Padding");
+        IvParameterSpec iv = new IvParameterSpec(Base64.getDecoder().decode(ivBase64));
+
+        cipher.init(Cipher.DECRYPT_MODE, Key, iv);
+        byte[] decryptedBytes = cipher.doFinal(Base64.getDecoder().decode(cipherTextBase64));
+
+        return new String(decryptedBytes);
+    }
 }
--- src/main/java/org/example/Entry.java
+++ src/main/java/org/example/Entry.java
@@ -1,4 +1,59 @@
 package org.example;
 
 public class Entry {
+    private String service;
+    private String username;
+    private String salt;
+    private String iv;
+    private String encryptedPassword;
+
+    public Entry(String service, String username, String salt, String iv, String encryptedPassword) {
+        this.service = service;
+        this.username = username;
+        this.salt = salt;
+        this.iv = iv;
+        this.encryptedPassword = encryptedPassword;
+    }
+
+    //Getters
+    public String getService() {
+        return service;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getSalt() {
+        return salt;
+    }
+
+    public String getIv() {
+        return iv;
+    }
+
+    public String getEncryptedPassword() {
+        return encryptedPassword;
+    }
+
+    //Setters
+    public void setService(String service) {
+        this.service = service;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public void setSalt(String salt) {
+        this.salt = salt;
+    }
+
+    public void setIv(String iv) {
+        this.iv = iv;
+    }
+
+    public void setEncryptedPassword(String encryptedPassword) {
+        this.encryptedPassword = encryptedPassword;
+    }
 }
